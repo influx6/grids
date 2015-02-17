d74bda8fd2b0eb0f9beb5d554c19919cf3795a00
Alexander Ewetumo <trinoxf@gmail.com>
2015-02-17 15:38:13 +0100

    binding changes

--- grids.go
+++ grids.go
@@ -6,102 +6,225 @@
 
 package grids
 
+import (
+	"github.com/influx6/evroll"
+)
+
 //Packet The packet structor to be sent along in every channel
-type Packet struct {
-	body interface{}
-}
+type Iv interface{}
+type PacketChannel chan interface{}
+type PacketMap map[string]PacketChannel
+type OperationFn func(i Iv, p Iv)
+type PacketMapOperation map[string]OperationFn
 
 //GridInterface is the interface that defines the method mainly used by Grids
 type GridInterface interface {
 	NewIn(f string)
 	NewOut(f string)
-	In(f string) (chan *Packet, bool)
-	Out(f string) (chan *Packet, bool)
-	DelIn(f string) bool
-	DelOut(f string) bool
+	In(f string)
+	Out(f string)
+	DelIn(f string)
+	DelOut(f string)
+	InStream(f string, f string) evroll.Roller
+	OutStream(f string, f string) evroll.Roller
 }
 
 //Grid struct is the real struct container for fbp blocks
 type Grid struct {
-	id          string
-	inChannels  map[string](chan *Packet)
-	outChannels map[string](chan *Packet)
-	mutator     func(g *Grid)
+	Id           string
+	InChannels   PacketMap
+	OutChannels  PacketMap
+	InFnChannel  PacketMapOperation
+	OutFnChannel PacketMapOperation
+}
+
+//Stream listens to a particular channel and collect the data sent into it and sends it into
+//a roller/middleware type of struct
+
+func (g *Grid) Stream(grp string, id string) evroll.Roller {
+
+	ev := evroll.NewRoller()
+
+	return ev
 }
 
 //newIn allows the addition of a new channel into the Grid in-comming channel list
-func (g *Grid) newIn(f string) {
-	c := make(chan *Packet)
-	g.inChannels[f] = c
+func (g *Grid) NewIn(f string, fn OperationFn) {
+	if _, ok := g.InChannels[f]; ok {
+		return
+	}
+	c := make(PacketChannel)
+	g.InChannels[f] = c
+	g.InFnChannel[f] = fn
 }
 
 //newOut allows the addition of a new channel into the Grid out-going channel list
-func (g *Grid) newOut(f string) {
-	c := make(chan *Packet)
-	g.outChannels[f] = c
+func (g *Grid) NewOut(f string, fn OperationFn) {
+	if _, ok := g.OutChannels[f]; ok {
+		return
+	}
+	c := make(PacketChannel)
+	g.OutChannels[f] = c
+	g.OutFnChannel[f] = fn
 }
 
 //delOut - deletes and runs a go closure to close the channel in the outgoing list
-func (g *Grid) delOut(f string) bool {
-	c, err := g.Out(f)
+func (g *Grid) DelOut(f string) bool {
+	c, _ := g.Out(f)
 
-	if !err {
+	if c == nil {
 		return false
 	}
 
 	go func() {
 		close(c)
+		delete(g.OutFnChannel, f)
 	}()
 
-	delete(g.outChannels, f)
+	delete(g.OutChannels, f)
 	return true
 }
 
 //delOut - deletes and runs a go closure to close the channel in the incoming list
-func (g *Grid) delIn(f string) bool {
-	c, err := g.In(f)
+func (g *Grid) DelIn(f string) bool {
+	c, _ := g.In(f)
 
-	if !err {
+	if c == nil {
 		return false
 	}
 
 	go func() {
 		close(c)
+		delete(g.InFnChannel, f)
 	}()
 
-	delete(g.inChannels, f)
+	delete(g.InChannels, f)
 	return true
 }
 
 //Out - grabs the channel tag by the specified key in the current Grid struct out-channels
-func (g *Grid) Out(f string) (chan *Packet, bool) {
-	c, ok := g.outChannels[f]
-	return c, ok
+func (g *Grid) Out(f string) (PacketChannel, OperationFn) {
+	if c, ok := g.OutChannels[f]; ok {
+		if d, ok := g.OutFnChannel[f]; ok {
+			return c, d
+		}
+	}
+	return nil, nil
 }
 
 //In - grabs the channel tag by the specified key in the current Grid struct in-channels
-func (g *Grid) In(f string) (chan *Packet, bool) {
-	c, ok := g.inChannels[f]
-	return c, ok
+func (g *Grid) In(f string) (PacketChannel, OperationFn) {
+	if c, ok := g.InChannels[f]; ok {
+		if d, ok := g.InFnChannel[f]; ok {
+			return c, d
+		}
+	}
+
+	return nil, nil
 }
 
 //String - returns the id of this grid
 func (g *Grid) String() string {
-	return g.id
+	return g.Id
 }
 
 //NewGrid - constructor method for creating new grid struct with a function passed in for modds
-func NewGrid(s string, f func(r *Grid)) *Grid {
-	g := &Grid{s, map[string](chan *Packet){}, map[string](chan *Packet){}, f}
-	g.mutator(g)
+func NewGrid(s string) *Grid {
+	g := &Grid{s, make(PacketMap), make(PacketMap), make(PacketMapOperation), make(PacketMapOperation)}
 	return g
 }
 
-//FromGrid - need to mutate a new grid of an old one,simple pass it in as argument and its
-//mutator function will be called on the new generated grid struct
-func FromGrid(s string, t *Grid, f func(r *Grid)) *Grid {
-	g := &Grid{s, map[string](chan *Packet){}, map[string](chan *Packet){}, f}
-	t.mutator(g)
-	g.mutator(g)
-	return g
-}
+//GridPlug handles the binding of two channels and also handles
+//managagement of ensure go routines for channel to channel
+//connection
+// type GridPlug struct {
+// 	From     *Grid
+// 	FromChan PacketChannel
+// 	ToChan   PacketChannel
+// 	operator OperationFn
+// 	active   bool
+// }
+//
+// func (p *GridPlug) operate() {
+// 	if !p.active {
+// 		return
+// 	}
+// 	go func() {
+// 		for d := range p.FromChan {
+// 			if !p.active {
+// 				break
+// 			}
+// 			p.operator(d, p.ToChan)
+// 		}
+// 	}()
+// }
+//
+// func (p *GridPlug) connect() {
+// 	if p.active {
+// 		return
+// 	}
+// 	p.active = true
+// 	go p.operate()
+// }
+//
+// func (p *GridPlug) disconnect() {
+// 	if !p.active {
+// 		return
+// 	}
+// 	p.active = false
+// }
+//
+// //BindInFunc will bind a in channel with a func
+// func BindInFunc(p GridInterface, pi string, fn OperationFn) (*GridPlug, bool) {
+// 	if ci, _ := p.In(pi); ci != nil {
+// 		return &GridPlug{p, ci, nil, fn, false}, true
+// 	}
+// 	return nil, false
+// }
+//
+// func BindOutFunc(p *Grid, pi string, fn OperationFn) (*GridPlug, bool) {
+// 	if ci, fn := p.Out(pi); ci != nil {
+// 		return &GridPlug{p, ci, nil, fn, false}, true
+// 	}
+// 	return nil, false
+// }
+//
+// //BindInOut will bind the in channel to the out channel of two grids
+// func BindInOut(p *Grid, pi string, f *Grid, fi string) (*GridPlug, bool) {
+// 	if ci, fn := p.In(pi); ci != nil {
+// 		if di, _ := f.Out(fi); di != nil {
+// 			return &GridPlug{p, ci, di, fn, false}, true
+// 		}
+// 	}
+// 	return nil, false
+// }
+//
+// //BindOutIn will bind the out channel to the in channel of two grids
+// func BindOutIn(p *Grid, pi string, f *Grid, fi string) (*GridPlug, bool) {
+// 	if ci, fn := p.Out(pi); ci != nil {
+// 		if di, _ := f.In(fi); di != nil {
+// 			return &GridPlug{p, ci, di, fn, false}, true
+// 		}
+// 	}
+// 	return nil, false
+// }
+//
+// //BindIn will bind both in channels of two grids together
+// func BindIn(p *Grid, pi string, f *Grid, fi string) (*GridPlug, bool) {
+// 	if ci, fn := p.In(pi); ci != nil {
+// 		if di, _ := f.In(fi); di != nil {
+// 			return &GridPlug{p, ci, di, fn, false}, true
+// 		}
+// 	}
+// 	return nil, false
+// }
+//
+// //BindOut will bind both out channels of two grids together
+// func BindOut(p *Grid, pi string, f *Grid, fi string) (*GridPlug, bool) {
+// 	if ci, fn := p.Out(pi); ci != nil {
+// 		if di, _ := f.Out(fi); di != nil {
+// 			return &GridPlug{p, ci, di, fn, false}, true
+// 		}
+// 	}
+// 	return nil, false
+// }
--- grids_test.go
+++ grids_test.go
@@ -6,35 +6,31 @@ import (
 
 func TestGridsNew(t *testing.T) {
 
-	g := NewGrid("servo.io", func(r *Grid) {
-		if r == nil {
-			t.Fatalf("cound not create Grid", r.id, r)
-		}
-	})
+	g := NewGrid("servo.io")
 
-	g.newIn("pull")
-	g.newOut("push")
+	g.NewIn("pull", func(i interface{}, p PacketChannel) {})
+	g.NewOut("push", func(i interface{}, p PacketChannel) {})
 
-	if _, ok := g.Out("push"); !ok {
+	if _, _, ok := g.Out("push"); !ok {
 		t.Fatalf("no channel called `push` in grid", ok, g)
 	}
 
-	if _, ok := g.In("pull"); !ok {
+	if _, _, ok := g.In("pull"); !ok {
 		t.Fatalf("no channel called `pull` in grid", ok, g)
 	}
 
-	if _, ok := g.In("push"); ok {
+	if _, _, ok := g.In("push"); ok {
 		t.Fatalf("theres a in-channel called `push` in grid", ok, g)
 	}
 
-	g.delIn("pull")
-	g.delOut("push")
+	g.DelIn("pull")
+	g.DelOut("push")
 
-	if _, ok := g.Out("push"); ok {
+	if _, _, ok := g.Out("push"); ok {
 		t.Fatalf("channel called `push` in grid not deleted", ok, g)
 	}
 
-	if _, ok := g.In("pull"); ok {
+	if _, _, ok := g.In("pull"); ok {
 		t.Fatalf("channel called `pull` in grid not deleted", ok, g)
 	}
 
