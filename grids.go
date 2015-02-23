7018b6542fb694fcc5fccefd0b8d631f85e0226c
Alexander Ewetumo <trinoxf@gmail.com>
2015-02-23 14:42:12 +0100

    switching from channels to rollers

--- grids.go
+++ grids.go
@@ -10,7 +10,6 @@ import (
 	// "fmt"
 	"github.com/influx6/evroll"
 	"github.com/influx6/immute"
-	"sync"
 )
 
 //Packet Are a combination of body map and a sequence list
@@ -21,8 +20,8 @@ type GridPacket struct {
 }
 
 //Packet The packet structor to be sent along in every channel
-type PacketChannel chan *GridPacket
-type PacketMap map[string]PacketChannel
+// type PacketChannel chan *GridPacket
+// type PacketMap map[string]PacketChannel
 type PacketMapRoller map[string]*evroll.Roller
 type GridMap map[interface{}]interface{}
 
@@ -63,40 +62,19 @@ type GridInterface interface {
 	Out(f string)
 	DelIn(f string)
 	DelOut(f string)
-	InStream(f string) evroll.Roller
-	OutStream(f string) evroll.Roller
-	MuxInStream(f string) evroll.Roller
-	MuxOutStream(f string) evroll.Roller
-	InBind(f string, c PacketChannel) evroll.Roller
-	OutBnd(f string, c PacketChannel) evroll.Roller
+	MuxIn(f string) evroll.Roller
+	MuxOut(f string) evroll.Roller
+	InBind(f string, c evroll.Roller) evroll.Roller
+	OutBnd(f string, c evroll.Roller) evroll.Roller
 	OutSend(f string)
 	InSend(f string)
-	Block()
-	Wait()
-	Unwait()
 }
 
 //Grid struct is the real struct container for fbp blocks
 type Grid struct {
-	Id               string
-	InChannels       PacketMap
-	OutChannels      PacketMap
-	InChannelRoller  PacketMapRoller
-	OutChannelRoller PacketMapRoller
-	Waitor           *sync.WaitGroup
-}
-
-//Wait boots up the Grid Workgroup to block and allow go routines to flow
-func (g *Grid) Block() {
-	g.Waitor.Add(1)
-}
-
-func (g *Grid) Wait() {
-	g.Waitor.Wait()
-}
-
-func (g *Grid) Unwait() {
-	defer g.Waitor.Done()
+	Id          string
+	InChannels  PacketMapRoller
+	OutChannels PacketMapRoller
 }
 
 //InSend sends data in functional style into a in-channel of the grid
@@ -107,16 +85,7 @@ func (g *Grid) InSend(id string, f *GridPacket) {
 		return
 	}
 
-	go func() {
-
-		g.Block()
-
-		go func() {
-			c <- f
-			defer g.Unwait()
-		}()
-
-	}()
+	c.RevMunch(f)
 }
 
 //OutSend sends data in functional style into a out-channel of the grid
@@ -127,76 +96,12 @@ func (g *Grid) OutSend(id string, f *GridPacket) {
 		return
 	}
 
-	go func() {
-
-		g.Block()
-
-		go func() {
-			c <- f
-			defer g.Unwait()
-		}()
-
-	}()
+	c.RevMunch(f)
 }
 
-//InBind provides a very easy means of functionally binding a channel into the callers grid in channel
-func (g *Grid) InBind(id string, c PacketChannel) *evroll.Roller {
-	if c == nil {
-		return nil
-	}
-
-	did := g.InStream(id)
-
-	if did == nil {
-		return nil
-	}
-
-	did.End(func(f interface{}, next func(i interface{})) {
-		fr, ok := f.(*GridPacket)
-
-		if !ok {
-			return
-		}
-
-		go func() {
-			c <- fr
-		}()
-		next(nil)
-	})
-
-	return did
-}
-
-//OutBind provides very easy means of functionally binding a channel into a out channel of a specific id in the caller Grid
-func (g *Grid) OutBind(id string, c PacketChannel) *evroll.Roller {
-	if c == nil {
-		return nil
-	}
-
-	did := g.OutStream(id)
-
-	if did == nil {
-		return nil
-	}
-
-	did.End(func(f interface{}, next func(i interface{})) {
-		fr, ok := f.(*GridPacket)
-
-		if !ok {
-			return
-		}
-		go func() {
-			c <- fr
-		}()
-		next(nil)
-	})
-
-	return did
-}
-
-//MutOutStream creates an ev.Roller extension of the in-channel single,cache ev.Roller to you can mutate of that data stream to create new interesting values for other grids to use,but there are no quick functional style as you will manual send the data into the channel yourself
-func (g *Grid) MuxInStream(id string) *evroll.Roller {
-	d := g.InStream(id)
+//MutIn creates an ev.Roller extension of the in-channel single,cache ev.Roller to you can mutate of that data stream to create new interesting values for other grids to use,but there are no quick functional style as you will manual send the data into the channel yourself
+func (g *Grid) MuxIn(id string) *evroll.Roller {
+	d := g.In(id)
 
 	if d == nil {
 		return nil
@@ -211,9 +116,9 @@ func (g *Grid) MuxInStream(id string) *evroll.Roller {
 	return ev
 }
 
-//MutOutStream creates an ev.Roller extension of the out-channel single,cache ev.Roller to you can mutate of that data stream to create new interesting values for other grids to use,but there are no quick functional style as you will manual send the data into the channel yourself
-func (g *Grid) MuxOutStream(id string) *evroll.Roller {
-	d := g.OutStream(id)
+//MutOut creates an ev.Roller extension of the out-channel single,cache ev.Roller to you can mutate of that data stream to create new interesting values for other grids to use,but there are no quick functional style as you will manual send the data into the channel yourself
+func (g *Grid) MuxOut(id string) *evroll.Roller {
+	d := g.Out(id)
 
 	if d == nil {
 		return nil
@@ -228,133 +133,68 @@ func (g *Grid) MuxOutStream(id string) *evroll.Roller {
 	return ev
 }
 
-//InStream listens to a particular in channel and collect the data sent into it and sends it into a roller/middleware type of struct
+//In listens to a particular in channel and collect the data sent into it and sends it into a roller/middleware type of struct
 
-func (g *Grid) InStream(id string) *evroll.Roller {
-	if r, ok := g.InChannelRoller[id]; ok {
+func (g *Grid) In(id string) *evroll.Roller {
+	if r, ok := g.InChannels[id]; ok {
 		return r
-	}
-
-	if c := g.In(id); c != nil {
-
-		ev := evroll.NewRoller()
-		ev.Or(func(_ interface{}) {
-			defer g.Unwait()
-		})
-
-		go func() {
-			g.Block()
-			for d := range c {
-				// g.Block()
-				ev.RevMunch(d)
-				// defer g.Unwait()
-			}
-		}()
-
-		g.InChannelRoller[id] = ev
-		return ev
 	}
 
 	return nil
 }
 
-//OutStream listens to a particular in channel and collect the data sent into it and sends it into a roller/middleware type of struct
+//Out listens to a particular in channel and collect the data sent into it and sends it into a roller/middleware type of struct
 
-func (g *Grid) OutStream(id string) *evroll.Roller {
-	if r, ok := g.OutChannelRoller[id]; ok {
+func (g *Grid) Out(id string) *evroll.Roller {
+	if r, ok := g.OutChannels[id]; ok {
 		return r
-	}
-
-	if c := g.Out(id); c != nil {
-
-		ev := evroll.NewRoller()
-		ev.Or(func(_ interface{}) {
-			defer g.Unwait()
-		})
-
-		// g.Block()
-
-		go func() {
-			g.Block()
-			for d := range c {
-				// g.Block()
-				ev.RevMunch(d)
-				// defer g.Unwait()
-			}
-			// defer g.UnWait()
-		}()
-
-		g.OutChannelRoller[id] = ev
-		return ev
 	}
 
 	return nil
 }
 
 //newIn allows the addition of a new channel into the Grid in-comming channel list
-func (g *Grid) NewIn(f string) {
-	if _, ok := g.InChannels[f]; ok {
+func (g *Grid) NewIn(id string) {
+	if _, ok := g.InChannels[id]; ok {
 		return
 	}
-	c := make(PacketChannel)
-	g.InChannels[f] = c
+
+	ev := evroll.NewRoller()
+
+	g.InChannels[id] = ev
+
 }
 
 //newOut allows the addition of a new channel into the Grid out-going channel list
-func (g *Grid) NewOut(f string) {
-	if _, ok := g.OutChannels[f]; ok {
+func (g *Grid) NewOut(id string) {
+	if _, ok := g.OutChannels[id]; ok {
 		return
 	}
-	c := make(PacketChannel)
-	g.OutChannels[f] = c
+
+	ev := evroll.NewRoller()
+
+	g.OutChannels[id] = ev
+
 }
 
 //delOut - deletes and runs a go closure to close the channel in the outgoing list
 func (g *Grid) DelOut(f string) bool {
-	c := g.Out(f)
-
-	if c == nil {
-		return false
+	if _, ok := g.OutChannels[f]; ok {
+		delete(g.OutChannels, f)
+		return true
 	}
 
-	go func() {
-		close(c)
-	}()
-
-	delete(g.OutChannels, f)
-	return true
+	return false
 }
 
 //delOut - deletes and runs a go closure to close the channel in the incoming list
 func (g *Grid) DelIn(f string) bool {
-	c := g.In(f)
-
-	if c == nil {
-		return false
+	if _, ok := g.InChannels[f]; ok {
+		delete(g.InChannels, f)
+		return true
 	}
 
-	go func() {
-		close(c)
-	}()
-
-	delete(g.InChannels, f)
-	return true
-}
-
-//Out - grabs the channel tag by the specified key in the current Grid struct out-channels
-func (g *Grid) Out(f string) PacketChannel {
-	if c, ok := g.OutChannels[f]; ok {
-		return c
-	}
-	return nil
-}
-
-//In - grabs the channel tag by the specified key in the current Grid struct in-channels
-func (g *Grid) In(f string) PacketChannel {
-	if c, ok := g.InChannels[f]; ok {
-		return c
-	}
-	return nil
+	return false
 }
 
 //String - returns the id of this grid
@@ -364,6 +204,6 @@ func (g *Grid) String() string {
 
 //NewGrid - constructor method for creating new grid struct with a function passed in for modds
 func NewGrid(s string) *Grid {
-	g := &Grid{s, make(PacketMap), make(PacketMap), make(PacketMapRoller), make(PacketMapRoller), new(sync.WaitGroup)}
+	g := &Grid{s, make(PacketMapRoller), make(PacketMapRoller)}
 	return g
 }
